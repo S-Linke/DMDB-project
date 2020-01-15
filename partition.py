b7e39bee1f8e9f96fe395f21745b29c495545590
S-Linke <59891584+S-Linke@users.noreply.github.com>
2020-01-15 11:53:12 +0100

    Add files via upload

---
+++ partition.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+
+
+def find_partitions(df, match_func):
+    """Recursive algorithm for finding duplicates in a DataFrame."""
+
+    def get_record_index(r):
+        return r[df.index.name or 'index']
+
+    # Records are easier to work with than a DataFrame
+    records = df.to_records()
+
+    # This is where we store each partition
+    partitions = []
+
+    def find_partition(at=0, partition=None, indexes=None):
+
+        r1 = records[at]
+
+        if partition is None:
+            partition = {get_record_index(r1)}
+            indexes = [at]
+
+        for i, r2 in enumerate(records):
+
+            if get_record_index(r2) in partition or i == at:
+                continue
+
+            if match_func(r1, r2):
+                partition.add(get_record_index(r2))
+                indexes.append(i)
+                find_partition(at=i, partition=partition, indexes=indexes)
+
+        return partition, indexes
+
+    while len(records) > 0:
+        partition, indexes = find_partition()
+        partitions.append(partition)
+        records = np.delete(records, indexes)
+
+    return pd.Series({
+        idx: partition_id
+        for partition_id, idxs in enumerate(partitions)
+        for idx in idxs
+    })
+
